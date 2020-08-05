5d025a873e9cd80d1392d5f1f4d503c98502c523
Joonas Leonsaari <joonas.leonsaari@gmail.com>
2020-08-05 23:50:36 +0300

    refactor(kill-count): refactor jquery to JS and remove the depedency

--- javascripts/kill-count.js
+++ javascripts/kill-count.js
@@ -1,22 +1,25 @@
-$().ready(() => {
-	$('.kill-count-table tr').each((i, element) => {
-		const boss = $(element).find('td:first-child');
-		const rank = parseInt(boss.html());
-		if (!isNaN(rank)) {
-			boss.addClass('rank ' + getRankClass(rank));
-		}
-	});
-	
-	function getRankClass(rank) {
-		if (rank <= 10) {
-			return 'legendary';
-		}
-		if (rank <= 100) {
-			return 'epic';
-		}
-		if (rank <= 250) {
-			return 'rare';
-		}
-		return 'uncommon';
-	}
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+    /* Array of world-ranks */
+    let worldRanks = document.getElementById('kill-count-table').getElementsByClassName('world-rank');
+
+    /* Loop through ranks & add a class depending on value */
+    for (let i = 0; i < worldRanks.length; i++) {
+        const rank = worldRanks[i].innerHTML; //the actual world-rank value
+        worldRanks[i].classList.add('rank-' + determineClassColor(rank));
+    }
+
+    function determineClassColor(rank) {
+        if (rank <= 10) {
+            return 'legendary';
+        }
+        if (rank <= 100) {
+            return 'epic';
+        }
+        if (rank <= 250) {
+            return 'rare';
+        }
+        return 'uncommon';
+    }
 });
