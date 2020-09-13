050ad8c34d41c25128c002a426eca3f24bc78cd0
Joonas Leonsaari <joonas.leonsaari@gmail.com>
2020-09-13 15:59:55 +0300

    fix(frontpage): mobile responsive

---
+++ javascripts/navigation.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("navigation").style.width = "75vw";
+    document.getElementById("navigation").style.padding = "15px 24px 24px 24px";
+
+}
+
+function closeNav() {
+    document.getElementById("navigation").style.width = "0";
+    setTimeout(function() { document.getElementById("navigation").style.padding = "15px 0 24px 0"; }, 50);
+}
